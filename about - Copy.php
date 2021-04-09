d3c3c5ab172b3e4b468e25a4d6032b86f029ce93
vaibhav-Tayal <karan@vridhisoftech.com>
2021-04-09 10:10:03 +0530

    about_copy

---
+++ about - Copy.php
@@ -0,0 +1,148 @@
+<?php include_once( "header.php") ?>
+<section class="inner-banner-sec" style="background-image: url('images/inner-page-banner.jpg');">
+  <div class="vertical-middle">
+    <div class="container">
+      <div class="banner-text">
+        <div class="row align-items-center">
+          <div class="col-lg-9">
+            <div class="inner-banner-title">
+              <h1>About Usssssssssssss</h1>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="breadcrumb">
+              <span>
+                <a href="#">
+                  <span><i class="fa fa-home" aria-hidden="true"></i> Home</span>
+                </a>
+              </span>><span>About us</span>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      
+    </div>
+  </div>
+</section>
+<!-- Section End -->
+<section class="comp-about-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+         <div class="about-images">
+          <img src="images/about-image.png">
+        </div>
+        <div class="about-description">
+          <div class="compan-year">SINCE 19955</div>
+          <div class="title-left">
+            <h2>About <span>Us</span></h2>
+          </div>
+          <p>COJA Projects is a leading project management company with the primary focus of helping industrial establishments to execute their projects by handling the design, construction, and maintenance of their capital projects by providing innovative solutions that ensure project certainty and capital efficiency. </p>
+          <p>Established in 2008, COJA Projects has been at the forefront of providing quality project management service deliveries for construction companies in need of competent and highly experienced professionals to manage their projects. </p>
+          <p>As a customer-centric project management company, our primary objective is to ensure the successful completion of the projects under our watch as efficiently as possible while saving your capital in the process. </p>
+          <p>With over 12 years of work experience and multiple successfully executed projects, we have the expertise to bring your project home irrespective of your industry. In the past 12 years, we have successfully served clients across a wide range of industries, particularly oil and gas, powerline, tunneling, marine construction, heavy civil and bridge construction companies.</p>
+          <p>If your goal is to accelerate the execution of your project from start to finish, then you can trust COJA Project as your progressive partner.</p>
+          <p>Our commitment to project management efficiency and prudence in handling capital has enabled us to gain the trust of our clients globally. Some of our numerous achievements as a project management company include the execution of WFMAC powerline, LNG plant, KEARL oilseeds and CNRL Horizon, to mention a few. </p>
+          <p>From <strong>planning construction</strong> to design and execution, we follow a well-rounded methodology to optimizes our partners’ productivity and efficiency without breaking banks. Our charges are affordable as we are able to handle all our services 100% online, from design review to field construction. Also, our fly-in fly-out approach helps you to save labor costs while employing the expertise of the industry’s best talents at our disposal. </p>
+          <p>Are you ready to take your project management to the next level? Contact us today to see how we are always available to bring the best out of your construction!</p>
+          <p><strong><i>COJA Projects, fully committed to cost-effective project management with 100% remote access.</i></strong></p> 
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Section End -->
+<section class="why-choose-us-sec grey-bg">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="title">
+          <h2>Why <span>Choose Us</span></h2>
+          <p>Project management companies abound, but what sets COJA Projects above the rest?</p>
+        </div>
+      </div>
+    </div>
+    <div class="row wcu-item-row">
+      <div class="col-lg-4">
+        <div class="wcu-item">
+          <img src="images/wcu-icon-a.png">
+          <h4>Low Overheads</h4>
+          <p>We are an industrial project management company that operates remotely. As a result, we incur minimal overhead costs.</p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="wcu-item">
+          <img src="images/wcu-icon-b.png">
+          <h4>Low Cost</h4>
+          <p>At COJA Projects, we are committed to ensuring our clients’ projects run as efficiently and effectively as possible. </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="wcu-item">
+          <img src="images/wcu-icon-c.png">
+          <h4>Unrivaled Expertise </h4>
+          <p>As a quality-minded company, our management team is made up of expert and highly qualified personnel.</p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="wcu-item">
+          <img src="images/wcu-icon-a.png">
+          <h4>Innovative Solutions</h4>
+          <p>We at COJA Projects place a high premium on innovation. Without it, we wouldn’t be nearly as successful at executing projects as we are. </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="wcu-item">
+          <img src="images/wcu-icon-b.png">
+          <h4>Quick Completion</h4>
+          <p>We understand that being a week late on completing a project can make a lot of difference to a company’s bottom line. </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="wcu-item">
+          <img src="images/wcu-icon-c.png">
+          <h4>Low Employee Turnover</h4>
+          <p>One of the problems that hinder the successful completion of most industrial projects is a high turnover rate. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Section End -->
+<section class="call-to-action-sec" style="background-image: url('images/call-to-action-bg.jpg');">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>We Are Ready To Work With You</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="#" class="btn btn-primary">Get A Quote</a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Section End -->
+<section class="client-logo-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="title">
+          <h2>Our <span>Partner</span></h2>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <ul class="client-logo-item">
+          <li><img src="images/icon1.png"></li>
+          <li><img src="images/icon2.png"></li>
+          <li><img src="images/icon3.png"></li>
+          <li><img src="images/icon4.png"></li>
+          <li><img src="images/icon5.png"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Section End -->
+<?php include_once("footer.php"); ?>
